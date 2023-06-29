51a4b29fde4b3b35026a108d1d87426bc4dbd8fe
Madhan-Rkv <madhanrkv10@gmail.com>
2023-06-29 16:00:30 +0530

    Age Calculator Completed

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -6,19 +6,18 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import com.google.android.material.datepicker.MaterialDatePicker
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    var selectedDateView: TextView? = null
-    var displayTextView: TextView? = null
+   private var selectedDateView: TextView? = null
+  private  var displayTextView: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val selectDateButton = findViewById<Button>(R.id.selectDate)
-        selectedDateView = findViewById<TextView>(R.id.selectDateView)
-        displayTextView = findViewById<TextView>(R.id.ageInMinutes)
+        selectedDateView = findViewById(R.id.selectDateView)
+        displayTextView = findViewById(R.id.ageInMinutes)
 
         selectDateButton.setOnClickListener {
             selectDate()
@@ -27,34 +26,47 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun selectDate() {
+    private fun selectDate() {
         val myCalender = Calendar.getInstance()
+        val calendar = Calendar.getInstance()
+        // Set the desired start year
+        calendar.set(Calendar.YEAR, 2020)
+        calendar.set(Calendar.MONTH, Calendar.JANUARY)
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
         val year = myCalender.get(Calendar.YEAR)
         val month = myCalender.get(Calendar.MONTH)
         val dayOfMonth = myCalender.get(Calendar.DAY_OF_MONTH)
-        DatePickerDialog(
+       val dialog= DatePickerDialog(
             this,
-            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+             { _, selectedYear, selectedMonth, selectedDayOfMonth ->
                 Toast.makeText(
                     this,
                     "$selectedYear-$selectedMonth-$selectedDayOfMonth",
                     Toast.LENGTH_LONG
                 ).show()
                 val selectedDates = "$selectedYear/$selectedMonth/$selectedDayOfMonth"
-                this.selectedDateView?.text = selectedDates
+               selectedDateView?.text = selectedDates
                 val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
                 val theDate = sdf.parse(selectedDates)
-                val dateInMinutes=theDate.time/60000
-                val currentDate=sdf.parse(sdf.format(System.currentTimeMillis()))
-                val currentTimeInMinutes=currentDate.time/60000
-                val diff=currentTimeInMinutes-dateInMinutes
-                displayTextView?.text=diff.toString()
+                theDate?.let {
+                    val dateInMinutes=theDate.time/60000
+                    val currentDate=sdf.parse(sdf.format(System.currentTimeMillis()))
+                    currentDate?.let {
+                        val currentTimeInMinutes=currentDate.time/60000
+                        val diff=currentTimeInMinutes-dateInMinutes
+                        displayTextView?.text=diff.toString()
+                    }
+                }
             },
             year,
             month,
             dayOfMonth
 
 
-        ).show()
+        )
+
+        dialog.datePicker.minDate = calendar.timeInMillis
+        dialog.datePicker.maxDate = myCalender.timeInMillis
+        dialog.show()
     }
 }
